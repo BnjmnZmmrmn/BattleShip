ea62913cff4e63c6bc47a6a8c7015d3678942f87
Ben <bz931@berkeley.edu>
2022-10-17 15:51:14 -0700

    Fixed author tag

--- src/Board.java
+++ src/Board.java
@@ -3,7 +3,7 @@ import java.util.Stack;
 
 /** This class contains the players ships and tracks hits,
  *  interfacing with Game.java.
- *  Author: Ben Zimmerman
+ *  @author Ben Zimmerman
  *  */
 public class Board {
 
