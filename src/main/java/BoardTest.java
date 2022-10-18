c2e3062339a0250750f6a1d2e34fff8dc5d47fe4
Ben <bz931@berkeley.edu>
2022-10-18 14:30:38 -0700

    imported junit testing via maven, changed file hierarchy entirely.  starting on BoardTest.java

---
+++ src/main/java/BoardTest.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void canPlace() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void placeShip() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void canMove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void move() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printBoard() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printHits() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void undo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void reset() {
+    }
+}
