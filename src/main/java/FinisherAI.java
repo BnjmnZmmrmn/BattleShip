0259a2b097fd2e8d47795ceb794243b6d569f836
Benjamin R. Zimmerman <bz931@berkeley.edu>
2023-03-26 15:16:07 -0700

    saving and pushing

--- src/main/java/FinisherAI.java
+++ src/main/java/FinisherAI.java
@@ -2,14 +2,31 @@
  * if it scores a hit, it tries to finish off the ship struck.
  *  @author Ben Zimmerman
  *  */
-public class FinisherAI extends RandAI{
+public class FinisherAI implements AI {
 
     /** AI's board */
     private Board _BOARD;
 
     /** Sets _BOARD */
     public FinisherAI(Board b) {
-        super(b);
+        _BOARD = b;
+    }
+
+    @Override
+    public void placeShips() {
+        int[] ships = {5, 4, 3, 3, 2};
+        char[] dirs = {'w', 'a', 's', 'd'};
+        for (int i = 0; i < ships.length; i ++) {
+            int len = ships[i];
+            int dI = (int)(Math.random() * 4);
+            int r = (int)(Math.random() * 10) + 1;
+            int c = (int)(Math.random() * 10) + 1;
+            while(!_BOARD.canPlace(len, dirs[dI], r + "" + c)) {
+                r = (int)(Math.random() * 10) + 1;
+                c = (int)(Math.random() * 10) + 1;
+            }
+            _BOARD.move(c + "" + r);
+        }
     }
 
     /** Makes moves randomly until it hits a ship, which it then
@@ -20,5 +37,10 @@ public class FinisherAI extends RandAI{
         int r = (int)(Math.random() * 10) + 1;
         int c = (int)(Math.random() * 10) + 1;
         return c + "" + r;
+    }
+
+    @Override
+    public Board getBoard() {
+        return _BOARD;
     }
 }
